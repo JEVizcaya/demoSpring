e70ee19ec9c0495ee2fa23e3ddf7191f4aa1999f
JEVizcaya <jevizcaya@hotmail.com>
2025-04-02 19:09:07 +0200

    estilos

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,19 +1,46 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    private PasswordEncoder encoder;
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(PasswordEncoder encoder, UserRepository userRepository) {
+        this.encoder = encoder;
+        this.userRepository = userRepository;
+    }
+
+
+
 
     @GetMapping("/login")
     public String login() {
         return "login";
     }
+
     @GetMapping("/register")
     public String register() {
         return "register";
     }
+
+    @PostMapping("/register")
+    public String register(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setRole("ROL_USER");
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+
 
 }
